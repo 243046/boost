8c2e63dff53b92c4cf05587887c8975e0c1c156f
243046 <243046@student.pwr.edu.pl>
2022-04-25 21:48:42 +0200

    Add AUC score to runners

--- experiments_runners/basic_runners.py
+++ experiments_runners/basic_runners.py
@@ -1,21 +1,34 @@
+from sklearn.metrics import make_scorer, roc_auc_score
+
 from wrappers.datasets_models_wrappers import DataModelsWrapper, DataModelsWrapperRandomSearch
 from wrappers.datasets_models_wrappers_nlp import DataModelsWrapperNLP, DataModelsWrapperNLPRandomSearch
+from utils.metrics import metric_f1_score
 
-
-def run(param_dict, mode='randomized', tuner='hyperopt'):
+def run(param_dict,
+        mode='randomized',
+        tuner='hyperopt',
+        final_scoring={'accuracy': 'accuracy',
+                       'f1_score': make_scorer(metric_f1_score),
+                       'AUC': make_scorer(roc_auc_score, needs_proba=True, multi_class='ovr', average='weighted')}
+    ):
     if mode == 'randomized':
-        model = DataModelsWrapperRandomSearch(param_dict)
+        model = DataModelsWrapperRandomSearch(param_dict, final_scoring=final_scoring)
     elif mode == 'TPE':
-        model = DataModelsWrapper(param_dict, tuner=tuner)
+        model = DataModelsWrapper(param_dict, tuner=tuner, final_scoring=final_scoring)
     model.fit()
     return model.all_datasets_results_, model.all_datasets_tuning_times_, model.all_datasets_runtimes_
 
 
-def run_nlp(param_dict, mode='randomized', tuner='hyperopt',
+def run_nlp(param_dict,
+            mode='randomized',
+            tuner='hyperopt',
+            final_scoring={'accuracy': 'accuracy',
+                           'f1_score': make_scorer(metric_f1_score),
+                           'AUC': make_scorer(roc_auc_score, needs_proba=True, multi_class='ovr', average='weighted')},
             tfidf_kws={'ngram_range': (1, 2), 'min_df': 3, 'max_features': 10000}):
     if mode == 'randomized':
-        model = DataModelsWrapperNLPRandomSearch(param_dict, tfidf_kws=tfidf_kws)
+        model = DataModelsWrapperNLPRandomSearch(param_dict, tfidf_kws=tfidf_kws, final_scoring=final_scoring)
     elif mode == 'TPE':
-        model = DataModelsWrapperNLP(param_dict, tuner=tuner, tfidf_kws=tfidf_kws)
+        model = DataModelsWrapperNLP(param_dict, tuner=tuner, tfidf_kws=tfidf_kws, final_scoring=final_scoring)
     model.fit()
     return model.all_datasets_results_, model.all_datasets_tuning_times_, model.all_datasets_runtimes_
