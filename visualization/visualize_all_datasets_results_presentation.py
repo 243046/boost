efd216dcc808d5dc8e3affd9200e1192e67035a3
243046 <243046@student.pwr.edu.pl>
2022-06-12 20:47:45 +0200

    Add presentation plots & final version of the thesis

---
+++ visualization/visualize_all_datasets_results_presentation.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from visualization.palettes import default_palette
+
+sns.set(font_scale=1.4)
+sns.set_style('whitegrid')
+
+
+def visualize_results_on_boxplots_facet(
+        results_path='../results/all_results.xlsx',
+        out_path='../plots/results_boxplots.pdf',
+        df=None,
+        metric='accuracy',
+        save=False,
+        **kwargs
+):
+    if df is None:
+        df = pd.read_excel(results_path)
+    df['dataset'] = df['dataset'].replace({
+        'adult': 'adult study',
+        'heart': 'heart disease',
+        'creditcard': 'credit card fraud',
+        'weather dataset': 'weather'
+    })
+    subset = ['adult study', 'amazon', 'churn', 'credit card fraud', 'weather', 'IMDB reviews']
+    df = df[df.dataset.isin(subset)]
+    melted = df.melt(id_vars='dataset', var_name='model', value_name=metric)
+    g = sns.catplot(data=melted, x='model', y=metric, col='dataset', kind='box', **kwargs)
+    g.set_titles(template='{col_name}', size=18, fontweight='bold')
+    if save:
+        plt.savefig(out_path, bbox_inches='tight', transparent=True)
+
+
+if __name__ == '__main__':
+    combinations = [
+        ('no_tuning_150_50_trees', '12_datasets_no_tuning_150_50_trees', 'accuracy'),
+        ('no_tuning_150_50_trees', '12_datasets_no_tuning_150_50_trees', 'f1_score'),
+        ('no_tuning_150_50_trees', '12_datasets_no_tuning_150_50_trees', 'AUC'),
+        ('TPE_tuning_150_50_trees', '12_datasets_TPE_150_50_trees', 'accuracy'),
+        ('TPE_tuning_150_50_trees', '12_datasets_TPE_150_50_trees', 'f1_score'),
+        ('TPE_tuning_150_50_trees', '12_datasets_TPE_150_50_trees', 'AUC'),
+        ('randomized_30_tuning_150_50_trees', '12_datasets_randomized_30_150_50_trees', 'accuracy'),
+        ('randomized_30_tuning_150_50_trees', '12_datasets_randomized_30_150_50_trees', 'f1_score'),
+        ('randomized_30_tuning_150_50_trees', '12_datasets_randomized_30_150_50_trees', 'AUC')
+    ]
+    for exp, name, scoring in combinations:
+        in_path = f'../results/basic_metrics_runtimes/{exp}/results_{scoring}_{name}.xlsx'
+        out_path = f'../presentation/{exp}/results_{scoring}_{name}_facet.pdf'
+        visualize_results_on_boxplots_facet(in_path,
+                                             out_path,
+                                             metric=scoring.replace('f1_', 'F1 '),
+                                             palette=default_palette,
+                                             col_wrap=3,
+                                             sharex=False,
+                                             sharey=False,
+                                             save=True
+                                             )
+
+
+
---
+++ visualization/visualize_all_datasets_runtimes_presentation.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import pandas as pd
+import seaborn as sns
+
+from visualization.palettes import default_palette
+
+sns.set(font_scale=1.4)
+sns.set_style('whitegrid')
+
+
+def visualize_runtimes_on_barplots_facet(
+        results_path='../results/all_runtimes.xlsx',
+        out_path='../plots/runtimes_barplots.pdf',
+        df=None,
+        time='runtime',
+        minutes=False,
+        save=False,
+        **kwargs
+):
+    if df is None:
+        df = pd.read_excel(results_path)
+    df['dataset'] = df['dataset'].replace({
+        'adult': 'adult study',
+        'heart': 'heart disease',
+        'creditcard': 'credit card fraud',
+        'weather dataset': 'weather'
+    })
+    subset = ['adult study', 'amazon', 'churn', 'credit card fraud', 'weather', 'IMDB reviews']
+    df = df[df.dataset.isin(subset)]
+    melted = df.melt(id_vars='dataset', var_name='model', value_name=time)
+    g = sns.catplot(data=melted, x='model', y=time, col='dataset', kind='bar', ci=None,  **kwargs)
+    g.set_titles(template='{col_name}', size=18, fontweight='bold')
+    for ax in g.axes.flatten():
+        if minutes:
+            ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: f'{int(y/60)}m'))
+        else:
+            ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: f'{int(y)}s'))
+        for c in ax.containers:
+            if minutes:
+                labels = [f'{v.get_height()/60:.3f}m' for v in c]
+            else:
+                labels = [f'{v.get_height():.3f}s' for v in c]
+            ax.bar_label(c, labels=labels, label_type='edge', fontsize=15)
+    if save:
+        plt.savefig(out_path, bbox_inches='tight', transparent=True)
+
+
+if __name__ == '__main__':
+    combinations = [
+        ('no_tuning_150_50_trees', '12_datasets_no_tuning_150_50_trees'),
+        ('TPE_tuning_150_50_trees', '12_datasets_TPE_150_50_trees'),
+        ('randomized_30_tuning_150_50_trees', '12_datasets_randomized_30_150_50_trees')
+    ]
+    times = ('runtime', 'tuning_time')
+    for time in times:
+        minutes = False if time == 'runtime' else True
+        for exp, name in combinations:
+            in_path = f'../results/basic_metrics_runtimes/{exp}/{time}s_{name}.xlsx'
+            out_path = f'../presentation/{exp}/{time}s_{name}_facet.pdf'
+            try:
+                visualize_runtimes_on_barplots_facet(in_path,
+                                                     out_path,
+                                                     time=time.replace('_', ' '),
+                                                     minutes=minutes,
+                                                     palette=default_palette,
+                                                     col_wrap=3,
+                                                     sharex=False,
+                                                     sharey=False,
+                                                     save=True
+                )
+            except:
+                print('no tuning times found')
